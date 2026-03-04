d1b743d8335946bd55dd51c232f8f5f9e57bd523
Cedrik Hoffmann <choffmann@progeek.de>
2026-03-04 13:48:43 +0100

    fix: add PATCH to cors

--- main.go
+++ main.go
@@ -721,7 +721,7 @@ func (c *Client) writePump() {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
